0be972527ad084a7c04cc3b139c53a3b9480b199
rajaravichandravelichety <rajaravichandravelichety@gmail.com>
2023-05-12 23:54:29 -0400

    added firebase and config for build process

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -13,6 +14,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/src/jquery.js';
 import 'popper.js/dist/popper.min.js';
 import VueAnimate from 'vue-animate-scroll'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDzw2-_RXD9dPlnwSZKnUHDfvm0M9lPhkk",
+  authDomain: "raja-velichety-portfolio.firebaseapp.com",
+  projectId: "raja-velichety-portfolio",
+  storageBucket: "raja-velichety-portfolio.appspot.com",
+  messagingSenderId: "1047169465300",
+  appId: "1:1047169465300:web:cf65dd5cca8c3f1b883441",
+  measurementId: "G-18T019EG79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 //side nav icons for social media and github
